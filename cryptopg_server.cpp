255245d0ef29932fc470e7f450643f51f945cb8d
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2020-11-06 00:46:17 -0800

    Created the skeleton code for public key retrieval service.

--- cryptopg_server.cpp
+++ cryptopg_server.cpp
@@ -2,12 +2,41 @@
 // Created by alfredd on 11/6/20.
 //
 
-#include <grpc++/grpc++.h>
+#include <grpcpp/grpcpp.h>
 #include "cryptopg.pb.h"
 #include "cryptopg.grpc.pb.h"
+
+
 #include <iostream>
+#include <memory>
+
+
+using namespace cryptopg;
+using namespace grpc;
+
+class CryptoPGServiceImpl : public CryptoPG::Service {
+public:
+	Status GetPKCS8PublicKey (ServerContext *context, const Dummy *request, PublicKey *response) override {
+		std::string responseKey("100100");
+		response->set_pkcs8publickey(responseKey);
+		return Status::OK;
+	}
+};
+
+void RunServer() {
+	string address ("0.0.0.0:45000");
+	CryptoPGServiceImpl service;
+	ServerBuilder serverBuilder;
+	serverBuilder.AddListeningPort(address, InsecureServerCredentials());
+	serverBuilder.RegisterService(&service);
+	std::unique_ptr<Server> server(serverBuilder.BuildAndStart());
+	std::cout<<"Server listening on " << address << std::endl;
+	server->Wait();
+}
+
 
 int main(int argc, char **argv)
 {
 	std::cout<<"GRPC CryptoPG Server"<<std::endl;
+	RunServer();
 }
