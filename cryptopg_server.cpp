577508f61d105fad895978dfeb26b5b457f1f2e0
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2020-11-06 00:31:38 -0800

    Creating a public key retrieval service with gRPC and cryptopp library.

---
+++ cryptopg_server.cpp
@@ -0,0 +1,13 @@
+//
+// Created by alfredd on 11/6/20.
+//
+
+#include <grpc++/grpc++.h>
+#include "cryptopg.pb.h"
+#include "cryptopg.grpc.pb.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout<<"GRPC CryptoPG Server"<<std::endl;
+}
