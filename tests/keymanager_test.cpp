827056dcfc34a7beec9c724f53d8e3558aa919a7
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2020-11-09 13:31:32 -0800

    Added test case to ensure that the key created by the KeyManager and saved on file is the same as the one read from the file.

--- tests/keymanager_test.cpp
+++ tests/keymanager_test.cpp
@@ -4,13 +4,33 @@
 
 #include "gtest/gtest.h"
 #include "../KeyManager.h"
+#include <cstdio>
 
 TEST(KeyManager, Test1) {
 	EXPECT_EQ(1,1);
 }
 
-TEST(KeyManager, Test2) {
+/*TEST(KeyManager, Test2) {
 	KeyManager keyManager;
 	std::string publicKey = keyManager.getPublicKey();
 	EXPECT_TRUE(publicKey.size()!=0);
+}*/
+
+void cleanup_files() {
+	std::remove("Key.priv");
+	std::remove("Key.pub");
+}
+
+TEST(KeyManager, Test3) {
+	cleanup_files();
+
+	KeyManager keyManager1;
+	std::string publicKey1 = keyManager1.getPublicKey();
+
+	KeyManager keyManager2;
+	std::string publicKey2 = keyManager2.getPublicKey();
+	EXPECT_TRUE(publicKey1==publicKey2);
+
+	cleanup_files();
+
 }
